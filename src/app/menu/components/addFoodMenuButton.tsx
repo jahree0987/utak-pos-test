5f1b88ce0644ef8ae99af89c2d5104dd9c490f70
Jahree Lester Chavez <jahreechavez022@gmail.com>
2024-03-06 01:20:11 +0800

    fix: build error on AddFoodMenuButton

--- src/app/menu/components/addFoodMenuButton.tsx
+++ src/app/menu/components/addFoodMenuButton.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import styled from "styled-components";
 
-export function AddFoodMenuButton({
+export default function AddFoodMenuButton({
   onClick,
   menuName,
 }: {
